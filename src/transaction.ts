20faa77856557b152468db6e7ae9275c106cd7bd
ptsayli <ptsayli@gmail.com>
2025-09-24 10:40:45 +0400

    minor fixes

--- src/transaction.ts
+++ src/transaction.ts
@@ -1,7 +1,7 @@
-import { CONFIG, ERRORS, NONCE_USED_ERROR } from "./constants"
-import { initRedis } from "./redis"
+import { Hex, PublicClient, WalletClient } from "viem"
 import * as CHAINS from "viem/chains"
-import { createPublicClient, Hex, http, PublicClient, WalletClient } from "viem"
+import { CONFIG, ERRORS } from "./constants"
+import { initRedis } from "./redis"
 import { _sleep } from "./utils"
 
 let chainMap = Object.keys(CHAINS).reduce((obj, key) => {
@@ -13,40 +13,54 @@ class Transaction {
   redisClient
   memoryStorage = new Map()
 
-  InitializeNonce = async (walletAddress, chainId) => {
+  keyNonce(address: string, chainId: number) {
+    return `nonce:${address.toLowerCase()}:${chainId}`
+  }
+
+  InitializeNonce = async (address, chainId, publicClient) => {
+    const walletAddress = address.toLowerCase()
     if (this.initializedNonces.get(walletAddress + chainId) !== undefined) return
     if (!this.redisClient && CONFIG.redis) {
       this.redisClient = await initRedis(CONFIG.redis)
     }
-    const chain = CHAINS[chainMap[chainId]]
-    const publicClient = createPublicClient({
-      chain,
-      transport: http(),
-    })
-    let pendingNonce = await publicClient.getTransactionCount({ address: walletAddress, blockTag: "latest" })
-    const key = `nonce:${walletAddress}:${chainId}`
-    if (this.initializedNonces.get(walletAddress + chainId) !== undefined) return
-
-    this.initializedNonces.set(walletAddress + chainId, pendingNonce)
-    if (this.redisClient) {
-      await this.redisClient.SET(key, pendingNonce)
-    } else {
-      this.memoryStorage.set(key, pendingNonce)
+    // const chain = CHAINS[chainMap[chainId]]
+    // const publicClient = createPublicClient({
+    //   chain,
+    //   transport: http(),
+    // })
+    let latestNonce = await publicClient.getTransactionCount({ address: walletAddress, blockTag: "latest" })
+    let pendingNonce = await publicClient.getTransactionCount({ address: walletAddress, blockTag: "pending" })
+    if ( pendingNonce> latestNonce) { 
+      // todo: send empty transaction
+      return { empty: true, nonce: latestNonce }
     }
-    return pendingNonce
-  }
-  GetNextNonce = async (walletAddress, chainId) => {
-    const key = `nonce:${walletAddress}:${chainId}`
+    // const key = `nonce:${walletAddress}:${chainId}`
+    const key = this.keyNonce(walletAddress, chainId)
+    if (this.initializedNonces.get(walletAddress + chainId) !== undefined) return
+    const seed = pendingNonce - 1
+    this.initializedNonces.set(walletAddress + chainId, seed)
     if (this.redisClient) {
-      return this.redisClient.incr(key, 1)
+      await this.redisClient.SET(key, seed)
     } else {
-      let nonce = this.memoryStorage.get(key)
-      this.memoryStorage.set(key, nonce + 1)
-      return nonce + 1
+      this.memoryStorage.set(key, seed)
+    }
+    return seed
+  }
+
+  GetNextNonce = async (walletAddress, chainId) => {
+    const key = this.keyNonce(walletAddress.toLowerCase(), chainId)
+    if (this.redisClient) {
+      return await this.redisClient.incrBy(key, 1)
+    } else {
+      let prev = this.memoryStorage.get(key)
+      if (typeof prev === "undefined") throw new Error("Nonce not initialized")
+      const next = prev + 1
+      this.memoryStorage.set(key, next)
+      return next
     }
   }
   GetCurrentNonce = async (walletAddress, chainId) => {
-    const key = `nonce:${walletAddress}:${chainId}`
+    const key = this.keyNonce(walletAddress.toLowerCase(), chainId)
     if (this.redisClient) {
       return await this.redisClient.get(key)
     } else {
@@ -54,31 +68,43 @@ class Transaction {
     }
   }
   DecreaseNonce = async (walletAddress, chainId) => {
-    const key = `nonce:${walletAddress}:${chainId}`
+    const key = this.keyNonce(walletAddress.toLowerCase(), chainId)
     if (this.redisClient) {
-      return await this.redisClient.incr(key, -1)
+      return await this.redisClient.decrBy(key, 1)
     } else {
-      let nonce = this.memoryStorage.get(key)
-      this.memoryStorage.set(key, nonce - 1)
-      return nonce
+      let currentNonce = this.memoryStorage.get(key)
+      if (typeof currentNonce === "undefined") return
+      const dec = currentNonce - 1
+      this.memoryStorage.set(key, dec)
+      return dec
     }
   }
+
+
   Send = async (props: { walletClient?: WalletClient; publicClient?: PublicClient<any, any, any>; tx: any; emptyTx?: boolean; chainId: number; tryCount: number; txHash?: string }) => {
     let { walletClient, publicClient, tx, emptyTx, chainId, tryCount, txHash } = props
+
     if (tryCount > CONFIG.MAX_TRY) return Promise.reject("Transaction Failed More than Max Try")
+
     CONFIG.log(`Trying nonce: ${tx.nonce}, tryCount ${tryCount}`)
+
     if (!tx.maxPriorityFeePerGas) {
       tx.maxPriorityFeePerGas = await publicClient.estimateMaxPriorityFeePerGas()
     } else if (tryCount !== 0) {
-      /** increase maxPriorityFeePerGas by 10% if retrying */
-      CONFIG.log("Increasing maxPriorityFeePerGas by 10%, nonce:", tx.nonce)
-      tx.maxPriorityFeePerGas = tx.maxPriorityFeePerGas + (tx.maxPriorityFeePerGas * 10) / 100
+      /** increase maxPriorityFeePerGas by 15% if retrying */
+      CONFIG.log("Increasing maxPriorityFeePerGas by 15%, nonce:", tx.nonce)
+      tx.maxPriorityFeePerGas = tx.maxPriorityFeePerGas + (tx.maxPriorityFeePerGas * BigInt(15)) / BigInt(100)
     }
     try {
       if (!txHash) {
         txHash = await walletClient.sendTransaction(tx)
       }
-      let res = await publicClient.waitForTransactionReceipt({ hash: txHash as Hex })
+      let res = await publicClient.waitForTransactionReceipt({
+        hash: txHash as Hex,
+        // confirmations: 1,
+        // timeout: 45000,
+        // pollingInterval: 1500,
+      })
       if (res.status !== "success") {
         /** if reverted */
         return Promise.reject(res)
@@ -94,7 +120,7 @@ class Transaction {
       if (!String(error).includes("Too Many Requests")) {
         tryCount++
       } else {
-        CONFIG.log("RPC Error at nonce", tx.nonce)
+        CONFIG.log(`RPC Error at nonce: ${tx.nonce}`, String(error))
       }
 
       await _sleep(1000)
@@ -112,40 +138,50 @@ class Transaction {
           return this.Send({ ...props, txHash, tx, emptyTx: true })
         }
       }
+      const NONCE_USED_ERROR = ["Try increasing the nonce", "nonce has already been used"]
+
       if (NONCE_USED_ERROR.some((errorString) => String(error).includes(errorString))) {
         // increase nonce
         let nextNonce
-        const match = String(error).match(/nonce too low: next nonce ([^]*?), tx nonce/)
-        if (match) {
-          const key = `nonce:${walletAddress}:${chainId}`
-          nextNonce = Number(match[1])
-          if (this.redisClient) {
-            await this.redisClient.SET(key, nextNonce)
-          } else {
-            this.memoryStorage.set(key, nextNonce)
-          }
+        // const match = String(error).match(/nonce too low: next nonce ([^]*?), tx nonce/)
+        // if (match) {
+        const key = this.keyNonce(walletAddress.toLowerCase(), chainId)
+        // nextNonce = Number(match[1])
+        const pending = await publicClient.getTransactionCount({ address: walletAddress, blockTag: "pending" })
+        const seed = pending - 1
+        if (this.redisClient) {
+          await this.redisClient.SET(key, seed)
         } else {
-          nextNonce = await this.GetNextNonce(walletAddress, chainId)
+          this.memoryStorage.set(key, seed)
         }
+        // }
+        nextNonce = await this.GetNextNonce(walletAddress, chainId)
+
         CONFIG.log(`Current Nonce ${tx.nonce} Retrying With Next nonce`, nextNonce)
         tx.nonce = nextNonce
       }
-      if (!String(error).includes("Too Many Requests")) {
-        CONFIG.log(tx.nonce, error)
-      }
+      // if (!String(error).includes("Too Many Requests")) { // duplicate with above
+      //   CONFIG.log(tx.nonce, error)
+      // }
       if (String(error).includes("Timed out while waiting for transaction")) {
         /** most probably transaction waiting in the mempool */
         txHash = ""
+        //todo: resendWithHigherFee
       }
       return this.Send({ ...props, tx, tryCount, txHash })
     }
   }
   sendTransaction = async ({ walletClient, publicClient, chainId, tx }: { walletClient?: WalletClient; publicClient?: PublicClient<any, any, any>; chainId: number; tx: any }) => {
     let walletAddress = walletClient?.account?.address || tx.account?.address
-    let nonce = (await this.InitializeNonce(walletAddress, chainId)) || (await this.GetNextNonce(walletAddress, chainId))
-
-    if (!tx.nonce) {
-      tx.nonce = tx.nonce || nonce
+    if (!walletAddress) throw new Error("Missing Account/wallet")
+    const nonZero = await this.InitializeNonce(walletAddress, chainId, publicClient)
+    // if (nonZero?.empty) {
+    //   console.log("Sending empty transaction to clear pending state")
+    //   tx = { to: tx.to, nonce: tx.nonce, value: 0 }
+    //   return this.Send({ walletClient, publicClient, tx, chainId, emptyTx: true, tryCount:0 })
+    // }
+    if (tx.nonce === undefined) {
+      tx.nonce = await this.GetNextNonce(walletAddress, chainId)
     }
     return this.Send({ walletClient, publicClient, tx, chainId, tryCount: 0 })
   }
